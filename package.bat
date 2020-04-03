38a1934752ee831672c31ed104f27c3629fd2714
Manuel Hernández <manherna@ucm.es>
2020-04-03 18:29:19 +0200

    Small changes to packaging.

--- package.bat
+++ package.bat
@@ -3,14 +3,16 @@
 :: Generates a zip using 7zip comandline application ::
 
 @echo off
-set %TEMPDIR=.\tmp
+set TEMPDIR=.\tmp
 set LIBSFOLDER=.\v-env\Lib\site-packages
+set ZIPNAME=UPLOAD.zip
 mkdir %TEMPDIR%
 
 rmdir /Q/S %LIBSFOLDER%\__pycache__
 xcopy /s /E /I /Y %LIBSFOLDER% %TEMPDIR%
-for %%i in (models.py logic.py persistence.py logger.py lambda_function.py) do xcopy /Y %%i %TEMPDIR%
+for %%i in (models.py logic.py persistence.py logger.py lambda_function.py config.yaml) do xcopy /Y %%i %TEMPDIR%
 
-"C:\Program Files\7-Zip\7z.exe" a UPLOAD.zip %TEMPDIR%\*
+del %ZIPNAME%
+"C:\Program Files\7-Zip\7z.exe" a %ZIPNAME% %TEMPDIR%\*
 
 rmdir /Q/S %TEMPDIR%
